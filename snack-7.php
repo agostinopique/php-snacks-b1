74feba81ecf846ea5a7601c584be481c633ff2f5
agostinopique <ago.pique@yahoo.it>
2022-06-07 16:25:41 +0200

    PHP Snack-7;

--- snack-7.php
+++ snack-7.php
@@ -1,6 +1,53 @@
 <?php 
 
-/* Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. */
+/* Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, surname e un array contenente i suoi vote scolastici. Stampare Nome, surname e la media dei vote di ogni alunno. */
+
+$students = [
+    [
+        "name" => "Francesco",
+        "surname" => "Ferdinando",
+        "vote" => [5, 6, 8, 9, 7, 7]
+    ],
+    [
+        "name" => "Carl",
+        "surname" => "Marx",
+        "vote" => [7, 9, 8, 9, 7, 8]
+    ],
+    [
+        "name" => "Wolfgang",
+        "surname" => "Mozart",
+        "vote" => [7, 6, 8, 9, 7, 7]
+    ],
+    [
+        "name" => "Ugo",
+        "surname" => "Foscolo",
+        "vote" => [5, 6, 8, 9, 7, 9]
+    ],
+    [
+        "name" => "Enrico",
+        "surname" => "Fermi",
+        "vote" => [6, 6, 8, 9, 8, 7]
+    ],
+];
+
+var_dump($students);
+
+function averageCalculator($voteArr){
+
+    $sumVote = 0;
+
+    for($i = 0; $i < count($voteArr); $i++){
+        $sumVote += $voteArr[$i];
+    }
+    $averageVote = $sumVote / count($voteArr);
+
+    if(!is_int($averageVote)){
+        return number_format($averageVote, 2, ',', '');
+    }
+
+    return $averageVote;
+}
+
 
 ?>
 
@@ -13,6 +60,11 @@
     <title>PHP Snack-7</title>
 </head>
 <body>
-    
+    <h1>Resoconto dell'alunno:</h1>
+    <ul>
+        <?php for($i = 0; $i < count($students); $i++): ?>
+            <li><?php echo $students[$i]['name']. ' '. $students[$i]['surname']. ' - '. averageCalculator($students[$i]["vote"]) ?></li>
+        <?php endfor?>
+    </ul>
 </body>
 </html>
